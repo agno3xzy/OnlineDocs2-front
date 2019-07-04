dd537d1530c39cf4c41899f0b1782e0cbac38ec4
kirwan <1024691015@qq.com>
2019-07-04 14:02:38 +0800

    v1.3

---
+++ src/components/document-explore.vue
@@ -0,0 +1,142 @@
+<template>
+<div>
+    <navmenu></navmenu>
+    <div id="editor">
+        <div id="title-group" align="left">
+            <i class="el-icon-arrow-left"></i> 
+            <span style="margin-left:10px;">{{docName}}</span>
+        </div>
+        <quill-editor
+            v-model="content"
+            ref="myQuillEditor"
+            :options="editorOption">
+        </quill-editor>
+    </div>
+</div>
+</template>
+
+<script>
+import navmenu from './nav-menu'
+import { CurentTime } from '../../static/js/DiffToStringArray'
+export default {
+    name: 'document-edit',
+    data() {
+        return {
+            docName: '',
+            content: '',
+            editorOption: { 
+                placeholder: "输入任何内容，支持html",
+                modules: {  
+                    toolbar: [
+                    ['bold', 'italic', 'underline', 'strike'],
+                    ['blockquote', 'code-block'],
+                    [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+                    [{ 'indent': '-1' }, { 'indent': '+1' }],
+                    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+                    [{ 'font': [] }],
+                    [{ 'color': [] }, { 'background': [] }],
+                    [{ 'align': [] }],
+                    ]
+                }
+            },
+            timer: ''
+        }
+    },
+    components: {navmenu},
+    methods: {
+        timeUpdate() {
+            this.$axios(
+            {
+                url:'/conflictHandle',
+                method:"post",
+                data:{
+                    opList: [],
+                    newPath: this.$route.query.newPath,
+                    oldPath: this.$route.query.oldPath,
+                    username: window.sessionStorage.username,
+                    timeStamp: CurentTime()
+                },
+                transformRequest: [function (data) {
+                // Do whatever you want to transform the data
+                let ret = ''
+                for (let it in data) {
+                // 如果要发送中文 编码 
+                    ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+                }
+                return ret
+                }],
+                headers: {
+                    'Content-Type':'application/x-www-form-urlencoded'
+                }
+            }).catch(error => {
+                console.log(error.message);
+            })
+            .then(response => {
+                this.$refs.myQuillEditor.quill.deleteText(0,this.content.length)
+                this.$refs.myQuillEditor.quill.insertText(0,response.data.content)
+            });
+        },
+    },
+    mounted() {
+        //与后端通讯 获取文件内容
+        //axios
+        this.$axios(
+        {
+            url:'/edit',
+            method:"post",
+            data:{
+                oldPath: this.$route.query.oldPath,
+                newPath: this.$route.query.newPath
+            },
+            transformRequest: [function (data) {
+            // Do whatever you want to transform the data
+            let ret = ''
+            for (let it in data) {
+            // 如果要发送中文 编码 
+                ret += encodeURIComponent(it) + '=' + encodeURIComponent(data[it]) + '&'
+            }
+            return ret
+            }],
+            headers: {
+                'Content-Type':'application/x-www-form-urlencoded'
+            }
+        }).catch(error => {
+            console.log(error.message);
+        })
+        .then(response => {
+            //this.content = response.data.content
+            this.$refs.myQuillEditor.quill.insertText(0,response.data.content)
+            this.$refs.myQuillEditor.quill.enable(false)
+            this.timer = setInterval(this.timeUpdate,5000)
+        });
+    },
+    beforeDestroy() {
+        clearInterval(this.timer)
+    }
+}
+</script>
+
+<style scoped>
+#editor{
+    position: absolute;
+    left: 0;
+    top: 60px;
+    bottom: 0;
+    right: 0;
+    background-color: #F7F7F7;
+}
+#title-group {
+    position: relative;
+    top: 20px;
+    left: 20px;
+}
+.quill-editor {
+    position:absolute;
+    left: 25%;
+    top: 15%;
+    min-height: 200px;
+    height: 400px;
+    width: 900px;
+    min-width: 900px;
+}
+</style>
---
+++ src/components/document-history.vue
@@ -0,0 +1,71 @@
+<template>
+<div>
+    <navmenu></navmenu>
+    <div id="editor">
+        <div id="title-group" align="left">
+            <i class="el-icon-arrow-left"></i> 
+            <span style="margin-left:10px;">{{docName}}</span>
+        </div>
+        <quill-editor
+            v-model="content"
+            ref="myQuillEditor"
+            :options="editorOption">
+        </quill-editor>
+    </div>
+</div>
+</template>
+<script>
+import navmenu from './nav-menu'
+export default {
+    name: 'document-history',
+    data() {
+        return {
+            content: '',
+            editorOption: { 
+                placeholder: "输入任何内容，支持html",
+                modules: {  
+                    toolbar: [
+                    ['bold', 'italic', 'underline', 'strike'],
+                    ['blockquote', 'code-block'],
+                    [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+                    [{ 'indent': '-1' }, { 'indent': '+1' }],
+                    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+                    [{ 'font': [] }],
+                    [{ 'color': [] }, { 'background': [] }],
+                    [{ 'align': [] }],
+                    ]
+                }
+            },
+        }
+    },
+    mounted() {
+        this.$refs.myQuillEditor.quill.enable(false)
+    },
+    components: { navmenu }
+}
+</script>
+
+<style scoped>
+#editor{
+    position: absolute;
+    left: 0;
+    top: 60px;
+    bottom: 0;
+    right: 0;
+    background-color: #F7F7F7;
+}
+#title-group {
+    position: relative;
+    top: 20px;
+    left: 20px;
+}
+.quill-editor {
+    position:absolute;
+    left: 35%;
+    top: 15%;
+    min-height: 200px;
+    height: 400px;
+    width: 900px;
+    min-width: 900px;
+}
+</style>
